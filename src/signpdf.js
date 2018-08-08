8c345f681d05da029aa04104edec8724eb2f8e08
Valery Buchinsky <djmute@abv.bg>
2018-08-08 15:25:39 +0300

    Add PDF creation with PDFKit in the tests

--- src/signpdf.js
+++ src/signpdf.js
@@ -1,12 +1,17 @@
 import forge from 'node-forge';
 
-const signpdf = (pdfBuffer, p12Buffer) => {
-    if (!(pdfBuffer instanceof Buffer)) {
-        throw new Error('PDF expected as Buffer.');
-    }
-    if (!(p12Buffer instanceof Buffer)) {
-        throw new Error('p12 certificate expected as Buffer.');
-    }
+export default {
+    BYTERANGE_PLACEHOLDER: '**********',
+    SIGNATURE_MAX_LENGTH: 8192,
+    PKCS12_CERT_BAG: '1.2.840.113549.1.12.10.1.3',
+    PKCS12_KEY_BAG: '1.2.840.113549.1.12.10.1.2',
+    
+    sign: (pdfBuffer, p12Buffer) => {
+        if (!(pdfBuffer instanceof Buffer)) {
+            throw new Error('PDF expected as Buffer.');
+        }
+        if (!(p12Buffer instanceof Buffer)) {
+            throw new Error('p12 certificate expected as Buffer.');
+        }
+    },
 };
-
-export default signpdf;
--- src/signpdf.test.js
+++ src/signpdf.test.js
@@ -1,17 +1,119 @@
 import signpdf from './signpdf';
+import PDFDocument from 'pdfkit';
 
-describe('Test pdfsign', () => {
+/**
+ * Adds the objects that are needed for Adobe.PPKLite to read the signature.
+ * Also includes a placeholder for the actual signature.
+ * Returns an Object with all the added PDFReferences.
+ * @param {PDFDocument} pdf 
+ * @param {string} reason 
+ * @returns {object}
+ */
+const addSignaturePlaceholder = (pdf, reason) => {
+    // Generate the signature placeholder
+    const signature = pdf.ref({
+        Type: 'Sig',
+        Filter: 'Adobe.PPKLite',
+        SubFilter: 'adbe.pkcs7.detached',
+        ByteRange: [
+            0,
+            signpdf.BYTERANGE_PLACEHOLDER,
+            signpdf.BYTERANGE_PLACEHOLDER,
+            signpdf.BYTERANGE_PLACEHOLDER,
+        ],
+        Contents: Buffer.from(String.fromCharCode(0).repeat(signpdf.SIGNATURE_MAX_LENGTH)),
+        Reason: new String(reason),
+        M: new Date(),
+    });
+
+    // Generate signature annotation widget
+    const widget = pdf.ref({
+        Type: 'Annot',
+        Subtype: 'Widget',
+        FT: 'Sig',
+        Rect: [0, 0, 0, 0],
+        V: signature,
+        T: new String('Signature1'),
+        F: 4,
+        P: pdf._root.data.Pages.data.Kids[0],
+    });
+    // Include the widget in a page
+    pdf._root.data.Pages.data.Kids[0].data.Annots = [
+        widget,
+    ];
+
+    // Create a form (with the widget) and link in the _root
+    const form = pdf.ref({
+        SigFlags: 3,
+        Fields: [widget],
+    });
+    pdf._root.data.AcroForm = form;
+
+    return {
+        signature,
+        form,
+        widget,
+    }
+}
+
+/**
+ * Creates a Buffer containing a PDF.
+ * Returns a Promise that is resolved with the resulting Buffer of the PDFDocument.
+ * @returns {Promise<Buffer>}
+ */
+const createPdf = () => {
+    return new Promise((resolve) => {
+        const pdf = new PDFDocument({
+            autoFirstPage: true,
+            size: 'A4',
+            layout: 'portrait',
+            bufferPages: true,
+        });
+        pdf.info.CreationDate = '';
+
+        // Add some content to the page
+        pdf
+            .fillColor('#333')
+            .fontSize(25)
+            .moveDown()
+            .text('node-signpdf');
+
+        // Collect the ouput PDF
+        // and, when done, resolve with it stored in a Buffer
+        const pdfChunks = [];
+        pdf.on('data', (data) => {
+            pdfChunks.push(data);
+        });
+        pdf.on('end', () => {
+            resolve(Buffer.concat(pdfChunks));
+        });
+
+        // Externally (to PDFKit) add the signature placeholder.
+        const refs = addSignaturePlaceholder(pdf, 'I am the author');
+        // Externally end the streams of the created objects.
+        // PDFKit doesn't know much about them, so it won't .end() them.
+        Object.keys(refs).forEach((key) => refs[key].end());
+
+        // Also end the PDFDocument stream.
+        // See pdf.on('end'... on how it is then converted to Buffer.
+        pdf.end();
+    });
+};
+
+describe('Test signpdf', () => {
     it('expects PDF to be Buffer', () => {
         expect(() => {
-            signpdf('non-buffer', Buffer.from(''));
+            signpdf.sign('non-buffer', Buffer.from(''));
         }).toThrow();
     });
     it('expects P12 certificate to be Buffer', () => {
         expect(() => {
-            signpdf(Buffer.from(''), 'non-buffer');
+            signpdf.sign(Buffer.from(''), 'non-buffer');
         }).toThrow();
     });
-    it('signs input PDF', () => {
+    it('signs input PDF', async (done) => {
+        const pdfBuffer = await createPdf();
         console.log('it does not yet sign...');
+        done();
     });
 });
