e02b8b0f88142c9840f25893b0ba1fec9d1632b0
Valery Buchinsky <djmute@abv.bg>
2018-08-08 16:17:22 +0300

    :sparkles: It's the whole signing process now

--- src/signpdf.js
+++ src/signpdf.js
@@ -5,6 +5,19 @@ const PKCS12_KEY_BAG = '1.2.840.113549.1.12.10.1.2';
 export const DEFAULT_BYTE_RANGE_PLACEHOLDER = '**********';
 export const DEFAULT_SIGNATURE_MAX_LENGTH = 8192;
 
+function pad2(num) {
+    const s = `0${num}`;
+    return s.substr(s.length - 2);
+}
+
+function strHex(s) {
+    let a = '';
+    for (let i = 0; i < s.length; i += 1) {
+        a += pad2(s.charCodeAt(i).toString(16));
+    }
+    return a;
+}
+
 export class signpdf {
 
     constructor() {
@@ -51,6 +64,60 @@ export class signpdf {
             pdf.slice(0, byteRangePos),
             Buffer.from(actualByteRange),
             pdf.slice(byteRangeEnd),
+        ]);
+
+        // Remove the placeholder signature
+        pdf = Buffer.concat([
+            pdf.slice(0, byteRange[1]),
+            pdf.slice(byteRange[2], byteRange[2] + byteRange[3]),
+        ]);
+
+        const forgeCert = forge.util.createBuffer(p12Buffer.toString('binary'));
+        const p12Asn1 = forge.asn1.fromDer(forgeCert);
+        const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, '');
+        // get bags by type
+        const certBags = p12.getBags({bagType: PKCS12_CERT_BAG})[PKCS12_CERT_BAG];
+        const keyBags = p12.getBags({bagType: PKCS12_KEY_BAG})[PKCS12_KEY_BAG];
+
+        const p7 = forge.pkcs7.createSignedData();
+        p7.content = forge.util.createBuffer(pdf.toString('binary'));
+        let last = certBags[0];
+        Object.keys(certBags).forEach((i) => {
+            p7.addCertificate(certBags[i].cert);
+            last = certBags[i];
+        });
+
+        p7.addSigner({
+            key: keyBags[0].key,
+            certificate: last.cert,
+            digestAlgorithm: forge.pki.oids.sha256,
+            authenticatedAttributes: [
+                {
+                    type: forge.pki.oids.contentType,
+                    value: forge.pki.oids.data,
+                }, {
+                    type: forge.pki.oids.messageDigest,
+                    // value will be auto-populated at signing time
+                }, {
+                    type: forge.pki.oids.signingTime,
+                    // value can also be auto-populated at signing time
+                    value: new Date(),
+                },
+            ],
+        });
+        p7.sign();
+
+        const raw = forge.asn1.toDer(p7.toAsn1()).getBytes();
+    
+        let signature = strHex(raw);
+        signature += Buffer
+            .from(String.fromCharCode(0).repeat(this.signatureMaxLength - raw.length))
+            .toString('hex');
+    
+        pdf = Buffer.concat([
+            pdf.slice(0, byteRange[1]),
+            Buffer.from(`<${signature}>`),
+            pdf.slice(byteRange[1]),
         ]);
 
         return pdf;
