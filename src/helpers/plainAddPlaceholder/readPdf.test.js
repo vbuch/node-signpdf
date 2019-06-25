6cf547c3c44bba3314d50e5fe2b2e6c73e372f64
Valery Buchinsky <djmute@abv.bg>
2019-06-25 10:22:47 +0300

    :white_check_mark: :camera_flash:

---
+++ src/helpers/plainAddPlaceholder/readPdf.test.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import readPdf from './readPdf';
+import SignPdfError from '../../SignPdfError';
+
+describe('readPdf', () => {
+    it('Errors an incrementally updated PDF is passed in', () => {
+        const pdf = fs.readFileSync(`${__dirname}/../../../resources/incrementally_signed.pdf`);
+
+        try {
+            readPdf(pdf);
+            expect('here').not.toBe('here');
+        } catch (e) {
+            expect(e instanceof SignPdfError).toBe(true);
+            expect(e.type).toBe(SignPdfError.TYPE_PARSE);
+            expect(e.message).toMatchSnapshot();
+        }
+    });
+});
