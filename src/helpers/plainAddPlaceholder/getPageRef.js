abea7ddd50d15f39d38427a6a10722dab9cb6abb
Valery Buchinsky <djmute@abv.bg>
2019-06-28 11:14:45 +0300

    :pencil:

--- src/helpers/plainAddPlaceholder/getPageRef.js
+++ src/helpers/plainAddPlaceholder/getPageRef.js
@@ -1,15 +1,19 @@
 import getPagesDictionaryRef from './getPagesDictionaryRef';
 import findObject from './findObject';
 
-const getPageRef = (pdf, info) => {
+/**
+ * Finds the reference to a page.
+ *
+ * @param {Buffer} pdfBuffer
+ * @param {Object} info As extracted from readRef()
+ */
+export default function getPageRef(pdfBuffer, info) {
     const pagesRef = getPagesDictionaryRef(info);
-    const pagesDictionary = findObject(pdf, info.xref, pagesRef);
+    const pagesDictionary = findObject(pdfBuffer, info.xref, pagesRef);
     const kidsPosition = pagesDictionary.indexOf('/Kids');
     const kidsStart = pagesDictionary.indexOf('[', kidsPosition) + 1;
     const kidsEnd = pagesDictionary.indexOf(']', kidsPosition);
     const pages = pagesDictionary.slice(kidsStart, kidsEnd).toString();
     const split = pages.trim().split(' ', 3);
     return `${split[0]} ${split[1]} ${split[2]}`;
-};
-
-export default getPageRef;
+}
