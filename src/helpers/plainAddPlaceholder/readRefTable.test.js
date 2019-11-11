5a74ac8743bc6a013101923b3c0e2fc752060f2c
Valery Buchinsky <djmute@abv.bg>
2019-11-11 13:21:45 +0200

    Snapshot the ref tables of all available PDFs

--- src/helpers/plainAddPlaceholder/readRefTable.test.js
+++ src/helpers/plainAddPlaceholder/readRefTable.test.js
@@ -1,13 +1,20 @@
 import fs from 'fs';
 import readRefTable from './readRefTable';
-import SignPdfError from '../../SignPdfError';
 
 describe('readRefTable', () => {
-    it('Expects to merge correctly the refTable of resources/signed-once.pdf', () => {
-        const pdf = fs.readFileSync(`${__dirname}/../../../resources/signed-once.pdf`);
-
-        let r = readRefTable(pdf);
-        expect(r.offsets.has(19)).toBe(true);
-        expect(r.offsets.get(19)).toBe(18860);
+    it('Expects to merge correctly the refTable of resources', () => {
+        [
+            'signed-once.pdf',
+            'signed-twice.pdf',
+            'contributing.pdf',
+            'formexample.pdf',
+            'incrementally_signed.pdf',
+            'signed.pdf',
+            'w3dummy.pdf',
+        ].forEach((fileName) => {
+            const pdf = fs.readFileSync(`${__dirname}/../../../resources/${fileName}`);
+            const r = readRefTable(pdf);
+            expect(r).toMatchSnapshot();
+        });
     });
 });
