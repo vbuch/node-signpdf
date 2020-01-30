0c3425d0b12b6e631ca75649d15492e769e9da4b
Valery Buchinsky <v.buchinsky@gaida.ai>
2020-01-30 11:13:37 +0200

    Fixed it. Need to create tests for it.

--- src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
+++ src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
@@ -8,17 +8,16 @@ jest.mock('./findObject', () => ({
 }));
 
 describe('createBufferPageWithAnnotation', () => {
-    it('should be tested', () => {
+    it('Reports unsupported feature', () => {
+        findObject.mockImplementation(() => {
+            console.log('aham');
+            return '/Annots [1 0 R]'
+        });
+        const info = {xref: {}};
+        info.xref.offsets = new Map();
+        info.xref.offsets.set(1, 1);
+        const buffer = createBufferPageWithAnnotation('pdf', info, '1 0 R', '2 0 R');
+        console.log(buffer.toString());
+        expect(true).toBe(false);
     });
-//     it('Reports unsupported feature', () => {
-//         findObject.mockImplementation(() => '/Annots Exists');
-//         try {
-//             createBufferPageWithAnnotation('pdf', 'info', 'pageRef', 'widget');
-//             expect('here').not.toBe('here');
-//         } catch (e) {
-//             expect(e instanceof SignPdfError).toBe(true);
-//             expect(e.type).toBe(SignPdfError.TYPE_PARSE);
-//             expect(e.message).toMatchSnapshot();
-//         }
-//     });
 });
