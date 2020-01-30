5db7a4c1c1c8b6e1ccd678fbf97930a8bf7ee344
Valery Buchinsky <djmute@abv.bg>
2020-01-30 20:37:08 +0200

    :white_check_mark: Test createBufferPageWithAnnotation

--- src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
+++ src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
@@ -8,16 +8,19 @@ jest.mock('./findObject', () => ({
 }));
 
 describe('createBufferPageWithAnnotation', () => {
-    it('Reports unsupported feature', () => {
-        findObject.mockImplementation(() => {
-            console.log('aham');
-            return '/Annots [1 0 R]\n/Something [ELSE HERE]'
-        });
+    it('Adds annotation to an existing array', () => {
+        findObject.mockImplementation(() => (
+            '/Annots [1 0 R]\n/Something [ELSE HERE]'
+        ));
         const info = {xref: {}};
         info.xref.offsets = new Map();
         info.xref.offsets.set(1, 1);
-        const buffer = createBufferPageWithAnnotation('pdf', info, '1 0 R', '2 0 R');
-        console.log(buffer.toString());
-        expect(true).toBe(false);
+        const buffer = createBufferPageWithAnnotation(
+            'pdf',
+            info,
+            '1 0 R',
+            '2 0 R',
+        );
+        expect(buffer.toString().indexOf('/Annots [1 0 R 2 0 R]')).not.toBe(-1);
     });
 });
