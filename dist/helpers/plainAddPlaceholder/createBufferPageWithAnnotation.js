1ebfd07303eda3ba5920b7e02b741e49b7e2a25b
Valery Buchinsky <djmute@abv.bg>
2020-02-06 22:07:14 +0200

    Fix when no /Annots (#59)
    * :bug: Fix for when there are no annotations
    
    * :bento: test with a user-provided PDF that used to break
    
    * :arrow_up: Updated packages

--- dist/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.js
+++ dist/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.js
@@ -14,17 +14,30 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const createBufferPageWithAnnotation = (pdf, info, pagesRef, widget) => {
   const pagesDictionary = (0, _findObject.default)(pdf, info.xref, pagesRef).toString(); // Extend page dictionary with newly created annotations
 
-  const annotsStart = pagesDictionary.indexOf('/Annots');
-  const annotsEnd = pagesDictionary.indexOf(']', annotsStart);
-  let annots = pagesDictionary.substr(annotsStart, annotsEnd - annotsStart + 1);
-  annots = annots.substr(0, annots.length - 1); // remove the trailing ]
+  let annotsStart, annotsEnd, annots;
+  annotsStart = pagesDictionary.indexOf('/Annots');
+
+  if (annotsStart > -1) {
+    annotsEnd = pagesDictionary.indexOf(']', annotsStart);
+    annots = pagesDictionary.substr(annotsStart, annotsEnd + 1 - annotsStart);
+    annots = annots.substr(0, annots.length - 1); // remove the trailing ]
+  } else {
+    annotsStart = pagesDictionary.length;
+    annotsEnd = pagesDictionary.length;
+    annots = '/Annots [';
+  }
 
   const pagesDictionaryIndex = (0, _getIndexFromRef.default)(info.xref, pagesRef);
   const widgetValue = widget.toString();
   annots = annots + ' ' + widgetValue + ']'; // add the trailing ] back
 
   const preAnnots = pagesDictionary.substr(0, annotsStart);
-  const postAnnots = pagesDictionary.substr(annotsEnd + 1);
+  let postAnnots = '';
+
+  if (pagesDictionary.length > annotsEnd) {
+    postAnnots = pagesDictionary.substr(annotsEnd + 1);
+  }
+
   return Buffer.concat([Buffer.from(`${pagesDictionaryIndex} 0 obj\n`), Buffer.from('<<\n'), Buffer.from(`${preAnnots + annots + postAnnots}\n`), Buffer.from('\n>>\nendobj\n')]);
 };
 
