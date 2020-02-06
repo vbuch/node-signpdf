189220f7d30595b175a41f3f28a10eb1f5f9188f
Valery Buchinsky <v.buchinsky@gaida.ai>
2020-02-06 10:58:49 +0200

    :bug: Fix for when there are no annotations

--- src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
+++ src/helpers/plainAddPlaceholder/createBufferPageWithAnnotation.test.js
@@ -23,4 +23,20 @@ describe('createBufferPageWithAnnotation', () => {
         );
         expect(buffer.toString().indexOf('/Annots [1 0 R 2 0 R]')).not.toBe(-1);
     });
+    it('Adds annotation when one does not exist', () => {
+        findObject.mockImplementation(() => (
+            '/There /Is /Other [] /Stuff /But /No /xxAnnotsxx'
+        ));
+        const info = {xref: {}};
+        info.xref.offsets = new Map();
+        info.xref.offsets.set(1, 1);
+        const buffer = createBufferPageWithAnnotation(
+            'pdf',
+            info,
+            '1 0 R',
+            '2 0 R',
+        );
+        const regex = /\/Annots\s+\[\s*2 0 R\s*\]/m;
+        expect(regex.test(buffer.toString())).toBe(true);
+    });
 });
