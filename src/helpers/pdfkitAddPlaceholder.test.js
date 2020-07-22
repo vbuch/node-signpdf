144840be26157faf24017ee9c6fc27e3a44f5d8c
Alexey Shirmanov <ashirman@users.noreply.github.com>
2020-07-22 10:11:32 +0300

    ✨ allow pdfkitAddPlaceholder to receive custom location, contactInfo, name (#87)

--- src/helpers/pdfkitAddPlaceholder.test.js
+++ src/helpers/pdfkitAddPlaceholder.test.js
@@ -22,4 +22,55 @@ describe('pdfkitAddPlaceholder', () => {
             '**********',
         ]);
     });
+
+    it('placeholder contains reason, contactInfo, name, location', () => {
+        const pdf = new PDFDocument({
+            autoFirstPage: true,
+            size: 'A4',
+            layout: 'portrait',
+            bufferPages: true,
+        });
+        pdf.info.CreationDate = '';
+
+        const refs = pdfkitAddPlaceholder({
+            pdf,
+            pdfBuffer: Buffer.from([pdf]),
+            reason: 'test reason',
+            contactInfo :'testemail@test.com',
+            name :'test name',
+            location : 'test Location'
+        });
+        expect(Object.keys(refs)).toMatchSnapshot();
+        expect(pdf.page.dictionary.data.Annots).toHaveLength(1);
+        expect(pdf.page.dictionary.data.Annots[0].data.Subtype).toEqual('Widget');
+        let widgetData = pdf.page.dictionary.data.Annots[0].data.V.data;
+        expect(widgetData['Reason']).toEqual('test reason')
+        expect(widgetData['ContactInfo']).toEqual('testemail@test.com')
+        expect(widgetData['Name']).toEqual('test name')
+        expect(widgetData['Location']).toEqual('test Location')
+    });
+
+    it('placeholder contains default values for contactInfo, name, location', () => {
+        const pdf = new PDFDocument({
+            autoFirstPage: true,
+            size: 'A4',
+            layout: 'portrait',
+            bufferPages: true,
+        });
+        pdf.info.CreationDate = '';
+
+        const refs = pdfkitAddPlaceholder({
+            pdf,
+            pdfBuffer: Buffer.from([pdf]),
+            reason: 'test reason'
+        });
+        expect(Object.keys(refs)).toMatchSnapshot();
+        expect(pdf.page.dictionary.data.Annots).toHaveLength(1);
+        expect(pdf.page.dictionary.data.Annots[0].data.Subtype).toEqual('Widget');
+        let widgetData = pdf.page.dictionary.data.Annots[0].data.V.data;
+        expect(widgetData['Reason']).toEqual('test reason')
+        expect(widgetData['ContactInfo']).toEqual('emailfromp1289@gmail.com')
+        expect(widgetData['Name']).toEqual('Name from p12')
+        expect(widgetData['Location']).toEqual('Location from p12')
+    });
 });
