9f55fa86b6278db50d9b79e626b3da9529f3e7ce
Valery Buchinsky <djmute@abv.bg>
2021-11-13 07:31:55 +0200

    📦 Build

--- dist/signpdf.js
+++ dist/signpdf.js
@@ -24,7 +24,6 @@ var _helpers = require("./helpers");
 Object.keys(_helpers).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
   if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
-  if (key in exports && exports[key] === _helpers[key]) return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function () {
@@ -38,7 +37,6 @@ var _const = require("./helpers/const");
 Object.keys(_const).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
   if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
-  if (key in exports && exports[key] === _const[key]) return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function () {
