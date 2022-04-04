8e2c35f49204c045c977f5fd30c02ef2a868365e
Valery Buchinsky <djmute@abv.bg>
2022-04-05 00:39:17 +0300

    Updated snapshot

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -2,6 +2,6 @@ import * as helpers from './index';
 
 describe('Helpers index', () => {
     it('Exports expected helpers', () => {
-        expect(Object.keys(helpers)).toMatchSnapshot();
+        expect(Object.keys(helpers).sort()).toMatchSnapshot();
     });
 });
