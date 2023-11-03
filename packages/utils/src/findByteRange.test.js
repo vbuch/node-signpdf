6b5e0bdefcfd095188997b34e9c8cf3482a94ee2
Valery Buchinsky <djmute@abv.bg>
2023-11-04 01:28:22 +0200

    ♻️  [utils] Reworked findByteRange
    to match more cases where it was incompatible so far (it didn't allow optional spaces in the array)

--- packages/utils/src/findByteRange.test.js
+++ packages/utils/src/findByteRange.test.js
@@ -1,4 +1,3 @@
-import PDFDocument from 'pdfkit';
 import {readTestResource} from '@signpdf/internal-utils';
 import {findByteRange} from './findByteRange';
 import {SignPdfError} from './SignPdfError';
@@ -13,46 +12,102 @@ describe(findByteRange, () => {
             expect(e.type).toBe(SignPdfError.TYPE_INPUT);
         }
     });
-    it('expects PDF to have a placeholder', () => {
-        try {
-            const pdf = new PDFDocument({
-                autoFirstPage: true,
-                size: 'A4',
-                layout: 'portrait',
-                bufferPages: true,
-            });
-            pdf.info.CreationDate = '';
-
-            findByteRange(Buffer.from([pdf]));
-            expect('here').not.toBe('here');
-        } catch (e) {
-            expect(e instanceof SignPdfError).toBe(true);
-            expect(e.type).toBe(SignPdfError.TYPE_PARSE);
-        }
-    });
-    it('expects to return correct byteRangeString and byteRange', async () => {
-        const pdfBuffer = readTestResource('with-placeholder.pdf');
-
-        const {byteRangePlaceholder, byteRanges} = findByteRange(pdfBuffer);
-
-        expect(byteRangePlaceholder).toBe('/ByteRange [0 /********** /********** /**********]');
-        expect(byteRanges[0][0]).toBe('0');
-        expect(byteRanges[0][1]).toBe('/**********');
-        expect(byteRanges[0][2]).toBe('/**********');
-        expect(byteRanges[0][3]).toBe('/**********');
-    });
-    it('expects byteRangePlaceholder to be undefined', async () => {
+    it('expects no byteRangePlaceholder but byteRangeStrings when PDF is already signed', async () => {
         const pdfBuffer = readTestResource('signed.pdf');
 
-        const {byteRangePlaceholder} = findByteRange(pdfBuffer);
+        const {byteRangePlaceholder, byteRangeStrings} = findByteRange(pdfBuffer);
 
         expect(byteRangePlaceholder).toBe(undefined);
-    });
-    it('expects byteRangeStrings to be pre-defined', async () => {
-        const pdfBuffer = readTestResource('signed.pdf');
-
-        const {byteRangeStrings} = findByteRange(pdfBuffer);
-
         expect(byteRangeStrings[0]).toBe('/ByteRange [0 153 3379 1275]');
     });
+    it('throws an error when multiple placeholder ranges are found', async () => {
+        const pdfBuffer = Buffer.from(`
+            /This (Is not an actual PDF)
+            /But (findByteRange shouldn't care)
+            /ByteRange [0 153 3379 1275]
+            /ByteRange [0 /********** /********** /**********]
+            /ByteRange [0   /**********  /********** /**********]
+            /ByteRange   [ 0 /AAA /AAA /AAA ]
+        `);
+
+        expect(() => {
+            findByteRange(pdfBuffer);
+        }).toThrowErrorMatchingInlineSnapshot('"Found multiple ByteRange placeholders."');
+    });
+    it('matches all byte ranges and recognizes the placeholder one', async () => {
+        const pdfBuffer = Buffer.from(`
+            /This (Is not an actual PDF)
+            /But (findByteRange shouldn't care)
+            /ByteRange [0 153 3379 1275]
+            /ByteRange [0 /********** /********** /**********]
+            /ByteRange   [ 0 /AAA /AAA /AAA ]
+        `);
+
+        const {byteRangePlaceholder, byteRangeStrings} = findByteRange(pdfBuffer, 'AAA');
+
+        expect(byteRangePlaceholder).toBe('/ByteRange   [ 0 /AAA /AAA /AAA ]');
+        expect(byteRangeStrings).toEqual(expect.arrayContaining([
+            '/ByteRange [0 153 3379 1275]',
+            '/ByteRange [0 /********** /********** /**********]',
+            '/ByteRange   [ 0 /AAA /AAA /AAA ]',
+        ]));
+    });
+
+    const specs = [
+        {
+            label: 'with omitted placeholder',
+            asString: '/ByteRange [0 /********** /********** /**********]',
+            match: '/ByteRange [0 /********** /********** /**********]',
+            byteRange: ['0', '/**********', '/**********', '/**********'],
+            // placeholder: optional,
+        },
+        {
+            label: 'with passed placeholder',
+            asString: '/ByteRange [0 /********** /********** /**********]',
+            match: '/ByteRange [0 /********** /********** /**********]',
+            byteRange: ['0', '/**********', '/**********', '/**********'],
+            placeholder: '**********',
+        },
+        {
+            label: 'ignoring spaces',
+            asString: '/ByteRange      [0   /**********  /**********           /**********]',
+            match: '/ByteRange      [0   /**********  /**********           /**********]',
+            byteRange: ['0', '/**********', '/**********', '/**********'],
+            placeholder: '**********',
+        },
+        {
+            label: 'with a different placeholder',
+            asString: '/ByteRange [ 0 /AAA /AAA /AAA ]',
+            match: '/ByteRange [ 0 /AAA /AAA /AAA ]',
+            byteRange: ['0', '/AAA', '/AAA', '/AAA'],
+            placeholder: 'AAA',
+        },
+        {
+            label: 'not just a placeholder',
+            asString: '/ByteRange [ 0 123 456 789 ]',
+            match: undefined,
+            byteRange: ['0', '123', '456', '789'],
+            placeholder: 'AAA',
+        },
+    ];
+    it.each(specs)(
+        'expects to return correct byteRangeString and byteRange - $label',
+        ({
+            byteRange, asString, placeholder, match,
+        }) => {
+            const pdfBuffer = Buffer.from(`
+                /This /Looks
+                /Like /A
+                /PDF /But
+                /Is /Not
+                ${asString}
+                /But (findByteRange shouldn't care)
+            `);
+
+            const {byteRangePlaceholder, byteRanges} = findByteRange(pdfBuffer, placeholder);
+
+            expect(byteRangePlaceholder).toBe(match);
+            expect(byteRanges[0]).toEqual(byteRange);
+        },
+    );
 });
