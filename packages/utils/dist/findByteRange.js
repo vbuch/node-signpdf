70a1fe07768b6db055fe412cf7946f210edf8beb
Valery Buchinsky <djmute@abv.bg>
2023-11-07 09:54:41 +0200

    📦 [utils]

--- packages/utils/dist/findByteRange.js
+++ packages/utils/dist/findByteRange.js
@@ -7,16 +7,25 @@ exports.findByteRange = void 0;
 var _SignPdfError = require("./SignPdfError");
 var _const = require("./const");
 /**
+* @typedef {object} OutputType
+* @property {string | undefined} byteRangePlaceholder
+* @property {number | undefined} byteRangePlaceholderPosition
+* @property {string[]} byteRangeStrings
+* @property {string[]} byteRange
+*/
+
+/**
  * Finds ByteRange information within a given PDF Buffer if one exists
  *
  * @param {Buffer} pdf
- * @returns {Object} {byteRangePlaceholder: String, byteRangeStrings: String[], byteRange: String[]}
+ * @returns {OutputType}
  */
 const findByteRange = (pdf, placeholder = _const.DEFAULT_BYTE_RANGE_PLACEHOLDER) => {
   if (!(pdf instanceof Buffer)) {
     throw new _SignPdfError.SignPdfError('PDF expected as Buffer.', _SignPdfError.SignPdfError.TYPE_INPUT);
   }
   let byteRangePlaceholder;
+  let byteRangePlaceholderPosition;
   const byteRangeStrings = [];
   const byteRanges = [];
   let offset = 0;
@@ -27,7 +36,7 @@ const findByteRange = (pdf, placeholder = _const.DEFAULT_BYTE_RANGE_PLACEHOLDER)
     }
     const rangeStart = pdf.indexOf('[', position);
     const rangeEnd = pdf.indexOf(']', rangeStart);
-    const byteRangeString = pdf.slice(position, rangeEnd + 1);
+    const byteRangeString = pdf.subarray(position, rangeEnd + 1);
     byteRangeStrings.push(byteRangeString.toString());
     const range = pdf.subarray(rangeStart + 1, rangeEnd).toString().split(' ').filter(c => c !== '').map(c => c.trim());
     byteRanges.push(range);
@@ -37,6 +46,7 @@ const findByteRange = (pdf, placeholder = _const.DEFAULT_BYTE_RANGE_PLACEHOLDER)
         throw new _SignPdfError.SignPdfError('Found multiple ByteRange placeholders.', _SignPdfError.SignPdfError.TYPE_INPUT);
       }
       byteRangePlaceholder = byteRangeString.toString();
+      byteRangePlaceholderPosition = position;
     }
     offset = rangeEnd;
 
@@ -44,6 +54,7 @@ const findByteRange = (pdf, placeholder = _const.DEFAULT_BYTE_RANGE_PLACEHOLDER)
   } while (true);
   return {
     byteRangePlaceholder,
+    byteRangePlaceholderPosition,
     byteRangeStrings,
     byteRanges
   };
