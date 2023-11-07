48d9385db870cbb69a127bc16fad0f175b71fe5b
Valery Buchinsky <djmute@abv.bg>
2023-11-07 09:56:28 +0200

    ⚡️ [signpdf] Use the ready BR position to spare a search

--- packages/signpdf/src/signpdf.test.js
+++ packages/signpdf/src/signpdf.test.js
@@ -82,7 +82,7 @@ describe('Test signing', () => {
         } catch (e) {
             expect(e instanceof SignPdfError).toBe(true);
             expect(e.type).toBe(SignPdfError.TYPE_PARSE);
-            expect(e.message).toMatchInlineSnapshot('"No ByteRangeStrings found within PDF buffer"');
+            expect(e.message).toMatchInlineSnapshot('"No ByteRangeStrings found within PDF buffer."');
         }
     });
     it('expects a reasonably sized placeholder', async () => {
