171c07ef296107ef6f3be74f9b98adecb9d5faf8
Valery Buchinsky <djmute@abv.bg>
2023-11-07 16:40:52 +0200

    🧑‍💻 [utils] Added ANNOTATION_FLAGS

--- packages/utils/src/const.js
+++ packages/utils/src/const.js
@@ -7,9 +7,8 @@ export const SUBFILTER_ADOBE_X509_SHA1 = 'adbe.x509.rsa.sha1';
 export const SUBFILTER_ETSI_CADES_DETACHED = 'ETSI.CAdES.detached';
 
 /**
- * Used in the /SigFlags as a bitmask.
+ * Signature flags (bitmask) to be used under /SigFlags.
  *
- * Flags specifying various document-level characteristics related to signature fields.
  * {@link https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/pdfreference1.3.pdf}
  * See TABLE 7.42 and 7.43
  * @readonly
@@ -25,4 +24,52 @@ export const SIG_FLAGS = {
      * if the file is saved (written) in a way that alters its previous contents.
      */
     APPEND_ONLY: 2,
+};
+
+/**
+ * Annotation flags (bitmask) to be used in /F under /Annot
+ *
+ * @readonly
+ * @enum {number}
+ * {@link https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/pdfreference1.3.pdf}
+ * See TABLE 7.10
+ */
+export const ANNOTATION_FLAGS = {
+    /**
+     * If set, do not display the annotation if it does not belong to one of the
+     * standard annotation types and no annotation handler is available.
+     */
+    INVISIBLE: 1,
+
+    /**
+     * If set, do not display or print the annotation or allow it to interact with the user,
+     * regardless of its annotation type or whether an annotation handler is available.
+     */
+    HIDDEN: 2,
+
+    /**
+     * If set, print the annotation when the page is printed. If clear, never print the
+     * annotation, regardless of whether it is displayed on the screen.
+     */
+    PRINT: 4,
+
+    /**
+     * If set, do not scale the annotation’s appearance to match the magnification of the page.
+     */
+    NO_ZOOM: 8,
+
+    /**
+     * If set, do not rotate the annotation’s appearance to match the rotation of the page.
+     */
+    NO_ROTATE: 16,
+
+    /**
+     * If set, do not display the annotation on the screen or allow it to interact with the user.
+     */
+    NO_VIEW: 32,
+
+    /**
+     * If set, do not allow the annotation to interact with the user.
+     */
+    READ_ONLY: 64,
 };
