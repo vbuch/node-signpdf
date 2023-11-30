823af74e215e0bfae5b3334be84802b9365a25b4
Valery Buchinsky <djmute@abv.bg>
2023-11-30 13:04:14 +0200

    🧪 Expect it to successfully parse a streamed resource that does not have the `trailer` keyword

--- packages/placeholder-plain/src/readPdf.test.js
+++ packages/placeholder-plain/src/readPdf.test.js
@@ -10,4 +10,12 @@ describe(readPdf, () => {
         expect(result.rootRef).toBe('12 0 R');
         expect(result.infoRef).toBe('1 0 R');
     });
+    it('reads issue-79-test.pdf', () => {
+        const pdfBuffer = readTestResource('issue-79-test.pdf');
+        const result = readPdf(pdfBuffer);
+
+        expect(result.xRefPosition).toBe(1542);
+        expect(result.rootRef).toBe('2 0 R');
+        expect(result.infoRef).toBe('3 0 R');
+    });
 });
